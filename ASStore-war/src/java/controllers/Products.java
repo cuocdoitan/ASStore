41590ab992aac79f25d6533064359a24d72d4b10
Nguyễn Việt Hưng <hunggarena12@gmail.com>
2018-05-04 20:45:43 +0700

    removed product.java

--- ASStore-war/src/java/controllers/Products.java
+++ ASStore-war/src/java/controllers/Products.java
@@ -7,22 +7,13 @@ package controllers;
 
 import SB.MediaFacadeLocal;
 import SB.ProductFacadeLocal;
-import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.PrintWriter;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import javax.ejb.EJB;
-import javax.imageio.ImageIO;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.Part;
 
 /**
  *
